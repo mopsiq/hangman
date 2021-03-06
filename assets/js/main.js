527370664f524e6c31625a701965f98afb3c9f62
mopsiq <60403947+mopsiq@users.noreply.github.com>
2021-03-06 18:07:24 +0200

    add window game && implementation random word in from the object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,89 @@
-import {addReturnButton, renderingGame} from './render.js';
+import {renderingGame, renderingStartPage} from './render.js';
 
 const startMenu = document.querySelector('.screensaver');
 const startButton = document.querySelector('.btn--start');
 const mainPage = document.querySelector('.page');
 
+const buttonReturn = document.createElement('div');
+
 startButton.addEventListener('click', () => {
-  addReturnButton(startMenu, mainPage, renderingGame);
+  startMenu.classList.add('hidden');
+  buttonReturn.classList.add('btn__return')
+  mainPage.append(buttonReturn)
+  // new enteringLiterals(mainPage)
+  renderingGame(mainPage)
+});
+
+buttonReturn.addEventListener('click', () => {
+  startMenu.classList.remove('hidden');
+  buttonReturn.classList.remove('btn__return');
+  renderingStartPage(document.querySelector('.game__window'));
+});
+
+
+mainPage.addEventListener('click', (e) => {
+  if(e.target.classList.contains('letter')) {
+    console.log(e.target)
+  }
 })
 
-console.log(startButton.clientHeight)
 
+
+
+// class enteringLiterals {
+//   constructor(mainBlock) {
+//     this.mainBlock = mainBlock;
+//     this.init()
+//   }
+
+//   init() {
+//     let mainDiv = document.createElement('div');
+//     let childrenDiv = document.createElement('div');
+//     mainDiv.classList.add('game__window');
+//     mainDiv.append(childrenDiv);
+//     childrenDiv.outerHTML = `
+//     <div class="game__letters">
+//       <button class="letter">А</button>
+//       <button class="letter">Б</button>
+//       <button class="letter">В</button>
+//       <button class="letter">Г</button>
+//       <button class="letter">Д</button>
+//       <button class="letter">Е</button>
+//       <button class="letter">Ё</button>
+//       <button class="letter">Ж</button>
+//       <button class="letter">З</button>
+//       <button class="letter">И</button>
+//       <button class="letter">Й</button>
+//       <button class="letter">К</button>
+//       <button class="letter">Л</button>
+//       <button class="letter">М</button>
+//       <button class="letter">Н</button>
+//       <button class="letter">О</button>
+//       <button class="letter">П</button>
+//       <button class="letter">Р</button>
+//       <button class="letter">С</button>
+//       <button class="letter">Т</button>
+//       <button class="letter">У</button>
+//       <button class="letter">Ф</button>
+//       <button class="letter">Х</button>
+//       <button class="letter">Ц</button>
+//       <button class="letter">Ч</button>
+//       <button class="letter">Ш</button>
+//       <button class="letter">Щ</button>
+//       <button class="letter">Ъ</button>
+//       <button class="letter">Ы</button>
+//       <button class="letter">Ь</button>
+//       <button class="letter">Э</button>
+//       <button class="letter">Ю</button>
+//       <button class="letter">Я</button>
+//     </div>
+//     `;
+//     this.mainBlock.append(mainDiv);
+//     console.log(mainDiv)
+//     mainDiv.addEventListener('click', (e) => this.test(e) )
+//   }
+
+//   test(e) {
+//     console.log(e.target)
+//   }
+// }
--- assets/js/render.js
+++ assets/js/render.js
@@ -1,16 +1,110 @@
-export function addReturnButton(btn, mainBlock, callback) {
-  btn.classList.add('hidden');
-  let returnBtn = document.createElement('div');
-  returnBtn.classList.add('btn__return');
-  mainBlock.append(returnBtn)
-  callback(mainBlock);
+// export function addReturnButton(btn, mainBlock, callback) {
+//   btn.classList.add('hidden');
+//   let returnBtn = document.createElement('div');
+//   returnBtn.classList.add('btn__return');
+//   mainBlock.append(returnBtn)
+//   callback(mainBlock);
+// }
+
+let objectWords = {
+  collection0: {
+    title: 'Животные',
+    words: ['слон', 'жираф']
+  },
+  collection1: {
+    title: 'Одежда',
+    words: ['шляпа', 'ботинок']
+  },
+  collection2: {
+    title: 'Электротехника',
+    words: ['стиральная машинка', 'компьютер']
+  },
 }
+
+function getWordInPage(obj, block) {
+  const randomValue = (data) => {
+    return Math.trunc(Math.random() * Object.keys(data).length)
+  };
+  let currentTheme = obj[`collection${randomValue(obj)}`];
+  let currentTitle = currentTheme.title;
+  let currentListWords = currentTheme.words;
+  let randomWord = currentListWords[randomValue(currentListWords)]
+  console.log(randomWord)
+}
+
+function getRandomValue(array) {
+  
+}
+
+getWordInPage(objectWords)
 
 export function renderingGame(mainBlock) {
   let mainDiv = document.createElement('div');
   let childrenDiv = document.createElement('div');
+  let lineInputDiv = document.createElement('div');
+
   mainDiv.classList.add('game__window');
+  mainDiv.append(lineInputDiv)
   mainDiv.append(childrenDiv);
-  childrenDiv.outerHTML = "<p>Text 1</p><p>Text 2</p><p>Text 3</p>";
-  mainBlock.append(mainDiv)
+
+  childrenDiv.outerHTML = `
+  <div class="game__letters">
+    <button class="letter">А</button>
+    <button class="letter">Б</button>
+    <button class="letter">В</button>
+    <button class="letter">Г</button>
+    <button class="letter">Д</button>
+    <button class="letter">Е</button>
+    <button class="letter">Ё</button>
+    <button class="letter">Ж</button>
+    <button class="letter">З</button>
+    <button class="letter">И</button>
+    <button class="letter">Й</button>
+    <button class="letter">К</button>
+    <button class="letter">Л</button>
+    <button class="letter">М</button>
+    <button class="letter">Н</button>
+    <button class="letter">О</button>
+    <button class="letter">П</button>
+    <button class="letter">Р</button>
+    <button class="letter">С</button>
+    <button class="letter">Т</button>
+    <button class="letter">У</button>
+    <button class="letter">Ф</button>
+    <button class="letter">Х</button>
+    <button class="letter">Ц</button>
+    <button class="letter">Ч</button>
+    <button class="letter">Ш</button>
+    <button class="letter">Щ</button>
+    <button class="letter">Ъ</button>
+    <button class="letter">Ы</button>
+    <button class="letter">Ь</button>
+    <button class="letter">Э</button>
+    <button class="letter">Ю</button>
+    <button class="letter">Я</button>
+  </div>
+  `;
+  lineInputDiv.outerHTML = `
+  <div class="game__line">
+  <span class="line line__hidden">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  <span class="line">a</span>
+  </div>
+  `;
+  mainBlock.append(lineInputDiv, mainDiv);
+}
+
+export function renderingStartPage(gameClass) {
+  gameClass.remove();
 }
